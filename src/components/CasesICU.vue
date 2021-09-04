388e66739740a4cee1f8773a8a8119a256748a49
MoAbyari <mo.abyari88@gmail.com>
2021-09-04 17:00:38 +1000

    Added CasesICU Component

---
+++ src/components/CasesICU.vue
@@ -0,0 +1,15 @@
+<template lang='html'>
+  <div>
+    <h1> Cases Coming soon </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CasesICU'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template lang='html'>
     <div>
         <Header />
+        <hr>
+        <CasesICU />
+
     </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import CasesICU from '@/components/CasesICU.vue';
 
 export default {
     name: 'Home',
     components: {
-        Header
+        Header,
+        CasesICU,
     }
 }
 </script>
