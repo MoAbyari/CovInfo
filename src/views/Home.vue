c5e2dbc8e53db36567bd6c440fb5c260182a5d9a
MoAbyari <mo.abyari88@gmail.com>
2021-09-04 16:50:53 +1000

    merged

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 import CasesICU from '@/components/CasesICU.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
