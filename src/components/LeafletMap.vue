443292c29dae0ea2fa9bda80d308fda4d99a5dfa
Hessam <tabatabaee.hessam@gmail.com>
2021-09-09 09:47:18 +1000

    min zoom implemented

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -30,7 +30,6 @@ export default {
                         d > 9   ?   'yellow' :
                                     '#f0f0f0';
         },
-
         styleMap(feature){
             if (feature?.properties.LGA_CODE19) {
                 const lgaCode = feature.properties.LGA_CODE19;
@@ -61,13 +60,14 @@ export default {
             }
         },
         setupLeafletMap: function () {
-            const mapDiv = L.map("mapContainer").setView(this.center, 7);
+            const mapDiv = L.map("mapContainer").setView(this.center, 8);
             L.tileLayer(
                 "https://api.mapbox.com/styles/v1/moabyari/ckt9sc7n2689r18pgaswrn4nl/tiles/{z}/{x}/{y}?access_token={accessToken}",
                 {
                     attribution:
                     'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
                     maxZoom: 16,
+                    minZoom: 7,
                     id: "mapbox/streets-v11",
                     accessToken: "pk.eyJ1IjoibW9hYnlhcmkiLCJhIjoiY2t0M3k3cngzMGNlcDJvbzJiZjByNHpydyJ9.UsxLK_4u6Ei3Ne_0gMUdrw",
                 }).addTo(mapDiv);
