2993d3a694318ebb7f78967ecfcac5640f9018aa
MoAbyari <mo.abyari88@gmail.com>
2021-09-10 00:50:54 +1000

    Updated CSS

--- src/components/UsefulInfo.vue
+++ src/components/UsefulInfo.vue
@@ -1,25 +1,47 @@
 <template>
-    <div>        
-        <button class="ui right labeled icon button" style="width: 80vw;">
-            <i class="right arrow icon"></i>
-            <a href="https://www.nsw.gov.au/covid-19/rules" > COVID-19 Rules </a>
-        </button>
+    <div class="useful-links"> 
+        <div class="usefullike-href">
+            <a href="https://www.nsw.gov.au/covid-19/health-and-wellbeing/covid-19-vaccination-nsw/get-your-covid-19-vaccination-nsw" > 
+                <button class="ui right labeled icon button" style="text-align:left; width: 80vw; background-color: white;background-color: white; border: 1px solid #757272;" >
+                    <i class="right arrow icon"></i>
+                    Get your COVID-19 vaccination 
+                </button> 
+            </a>  
+        </div>        
+        <div class="usefullike-href">
+            <a href="https://www.nsw.gov.au/covid-19/health-and-wellbeing/clinics" > 
+                <button class="ui right labeled icon button" style="text-align:left; width: 80vw; background-color: white;background-color: white; border: 1px solid #757272;" >
+                    <i class="right arrow icon"></i>
+                    COVID-19 testing clinics
+                </button> 
+            </a>  
+        </div>        
+        <div class="usefullike-href">
+            <a href="https://www.nsw.gov.au/covid-19/rules" > 
+                <button class="ui right labeled icon button" style="text-align:left; width: 80vw; background-color: white;background-color: white; border: 1px solid #757272;" >
+                    <i class="right arrow icon"></i>
+                    COVID-19 Rules 
+                </button> 
+            </a>  
+        </div>        
     </div>
 </template>
 
 <script>
 
-export default {
-    name: 'info',
-    data() {
-
-  }
-}
 </script>
 
 
 <style>
-    .action{
-        min-width: 90vw;
+    .useful-links{
+        width: 90vw;
+        margin: 0 auto;
+        padding: 30px 0px 30px 20px;
     }
+    .usefullike-href{
+        padding: 5px;
+        text-align: left;
+        margin: 0 auto;
+    }
+
 </style>
